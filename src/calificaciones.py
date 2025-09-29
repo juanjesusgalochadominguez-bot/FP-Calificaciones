486d067d94bda147e820698922b8e5ac4c941749
Juan Jesús <juanjesusgalochadominguez@gmail.com>
2025-09-29 10:21:44 +0200

    apartados hasta el c

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -15,3 +15,36 @@ def a_cero_alt(valor):
     if valor is None:
         return 0
     return valor
+
+def nota_cuatrimestre(nota_t1, nota_t2, nota_p):
+    
+    t1=a_cero(nota_t1)
+    t2=a_cero(nota_t2)
+    p=a_cero(nota_p)
+
+    media_teoria = nota_teoria(t1, t2)
+    if media_teoria < 4:
+        nota_cuatri = 0
+        return nota_cuatri
+    
+    nota_cuatri = 0.15*t1 + 0.15*t2 + 0.7*p
+    return nota_cuatri
+
+def nota_continua(lista_t, lista_p):
+    
+    for i in range(len(lista_t)):
+        lista_t[i] = a_cero(lista_t[i])
+        i=i+1
+    for j in range(len(lista_p)):
+        lista_p[j] = a_cero(lista_p[j])
+        j=j+1
+
+    nota_cuatri1 = nota_cuatrimestre(lista_t[0], lista_t[1], lista_p[0])
+    nota_cuatri2 = nota_cuatrimestre(lista_t[2], lista_t[3], lista_p[1])
+
+    if nota_cuatri1 < 5 or nota_cuatri2 < 5:
+        nota_curso = min(4.0, (nota_cuatri1 + nota_cuatri2)/2)
+        return nota_curso
+    
+    nota_curso = (nota_cuatri1 + nota_cuatri2)/2
+    return nota_curso
--- src/calificaciones_test.py
+++ src/calificaciones_test.py
@@ -18,3 +18,33 @@ print(nota_teoria(None, 3.0))
 print(nota_teoria(9.0, None))
 
 print(nota_teoria(None, None))
+
+#9.1, 7.2, 8.1 ==> 8.110000000000001
+#4.0, 6.0, 5.0 ==> 5.0
+#4.0, 3.0, None ==> 0
+#None, 3.0, None  ==>0
+#9.0, None, 4.5 ==> 4.5
+#9.0, None, None ==> 1.3499999999999999
+#None, None, None ==> 0
+
+print(nota_cuatrimestre(9.1, 7.2, 8.1))
+print(nota_cuatrimestre(4.0, 6.0, 5.0))
+print(nota_cuatrimestre(4.0, 3.0, None))
+print(nota_cuatrimestre(None, 3.0, None))
+print(nota_cuatrimestre(9.0, None, 4.5))
+print(nota_cuatrimestre(9.0, None, None))
+print(nota_cuatrimestre(None, None, None))
+
+
+#notas teoría:  9.6, 9.9, 10.0, 9.8 notas_práctico: 9.7, 9.5 ==> 9.6675
+#notas teoría: 4.4, 4.9, 5.1, 4.7 notas_práctico: 4.6, 4.8 ==> 4.0
+#notas teoría: 4.0, 6.0, 4.0, 3.0 notas_práctico: 5.0, None ==> 2.5
+#notas teoría: 9.0, None, 4.0, 3.0 notas_práctico: 4.5, None ==> 2.25
+#notas teoría: 9.0, 5.0, 4.0, None notas_práctico: 4.5, None ==> 2.25
+
+print("Notas continuas")
+print(nota_continua([9.6, 9.9, 10.0, 9.8], [9.7, 9.5]))
+print(nota_continua([4.4, 4.9, 5.1, 4.7], [4.6, 4.8]))
+print(nota_continua([4.0, 6.0, 4.0, 3.0], [5.0, None]))
+print(nota_continua([9.0, None, 4.0, 3.0], [4.5, None]))   
+print(nota_continua([9.0, 5.0, 4.0, None], [4.5, None]))
